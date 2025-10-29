45185916102ddfa54792d3e003b859d86cd1b1cc
Frank van Lankvelt <frank@vanlankvelt.net>
2025-10-29 12:26:24 +0100

    initial commit - get build going

---
+++ main.cc
@@ -0,0 +1,11 @@
+#include <cassert>
+#include "rocksdb/db.h"
+
+int main() {
+  rocksdb::DB* db;
+  rocksdb::Options options;
+  options.create_if_missing = true;
+  rocksdb::Status status =
+    rocksdb::DB::Open(options, "/tmp/testdb", &db);
+  assert(status.ok());
+}
